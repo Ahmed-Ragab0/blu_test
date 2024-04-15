89bb47d497f9c5241c8abd96fd8537c25acfb0b7
Ahmed-Ragab0 <ahmedragabmedo321@gmail.com>
2024-04-15 21:52:56 +0200

    last edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,226 +1,92 @@
+// Copyright 2017-2023, Charles Weinberger & Paul DeMarco.
+// All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-void main() => runApp(MyApp());
+import 'screens/bluetooth_off_screen.dart';
+import 'screens/scan_screen.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: BluetoothScreen(),
-    );
-  }
+void main() {
+  FlutterBluePlus.setLogLevel(LogLevel.verbose, color: true);
+  runApp(const FlutterBlueApp());
 }
 
-class BluetoothScreen extends StatefulWidget {
+//
+// This widget shows BluetoothOffScreen or
+// ScanScreen depending on the adapter state
+//
+class FlutterBlueApp extends StatefulWidget {
+  const FlutterBlueApp({Key? key}) : super(key: key);
+
   @override
-  _BluetoothScreenState createState() => _BluetoothScreenState();
+  State<FlutterBlueApp> createState() => _FlutterBlueAppState();
 }
 
-class _BluetoothScreenState extends State<BluetoothScreen> {
-  FlutterBlue flutterBlue = FlutterBlue.instance;
-  StreamSubscription<BluetoothDeviceState>? deviceConnection;
-  BluetoothDevice? device;
-  List<int> receivedData = [];
-  final double threshold = 80;
+class _FlutterBlueAppState extends State<FlutterBlueApp> {
+  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
 
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-      FlutterLocalNotificationsPlugin();
+  late StreamSubscription<BluetoothAdapterState> _adapterStateStateSubscription;
 
   @override
   void initState() {
     super.initState();
-    _initializeNotifications();
-    _requestNotificationPermission();
-    // Start scanning for Bluetooth devices
-    //_startScanning();
-  }
-
-  Future<void> _requestNotificationPermission() async {
-    if (await Permission.notification.isGranted) {
-      // Notification permission is already granted
-      return;
-    }
-    // Request notification permission
-    final permissionStatus = await Permission.notification.request();
-    if (permissionStatus != PermissionStatus.granted) {
-      // Permission not granted, show a message to the user
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content:
-              Text('Notification permission is required to receive alerts.'),
-        ),
-      );
-    }
-  }
-
-  Future<void> _initializeNotifications() async {
-    const AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings('@drawable/ic_launcher');
-    final InitializationSettings initializationSettings =
-        InitializationSettings(android: initializationSettingsAndroid);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-  }
-
-  void _startScanning() async {
-    try {
-      // Check if location permission is granted
-      if (await Permission.location.isGranted) {
-        // Location permission is granted, proceed with Bluetooth scanning
-        bool isBluetoothOn = await FlutterBlue.instance.isOn;
-        if (!isBluetoothOn) {
-          // Bluetooth is not enabled, prompt the user to enable it
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text('Please enable Bluetooth to start scanning.'),
-            ),
-          );
-          return;
-        }
-
-        // Indicate to the user that scanning has started
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Scanning for devices...'),
-          ),
-        );
-
-        // Start scanning for Bluetooth devices
-        flutterBlue.startScan(timeout: Duration(seconds: 4));
-        flutterBlue.scanResults.listen((results) {
-          // Look for your ESP32 device here
-          for (ScanResult r in results) {
-            if (r.device.name == 'Helmet') {
-              // Connect to the device
-              deviceConnection = r.device.state.listen((state) {
-                if (state == BluetoothDeviceState.connected) {
-                  // Device connected, start reading data
-                  device = r.device;
-                  _startReading();
-                }
-              });
-              flutterBlue.stopScan();
-              break;
-            }
-          }
-        });
-      } else {
-        // Location permission is not granted, request it
-        await Permission.location.request();
+    _adapterStateStateSubscription = FlutterBluePlus.adapterState.listen((state) {
+      _adapterState = state;
+      if (mounted) {
+        setState(() {});
       }
-    } catch (e) {
-      print('Error starting scan: $e');
-      // Show an error message to the user
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Failed to start scanning: $e'),
-        ),
-      );
-    }
-  }
-
-  void _startReading() async {
-    List<BluetoothService> services = await device!.discoverServices();
-    services.forEach((service) {
-      service.characteristics.forEach((characteristic) {
-        if (characteristic.properties.read) {
-          characteristic.value.listen((value) {
-            setState(() {
-              receivedData.addAll(value);
-              if (receivedData.length >= 5) {
-                processReceivedData(receivedData);
-                receivedData.clear();
-              }
-            });
-          });
-        }
-      });
     });
-  }
-
-  void processReceivedData(List<int> data) {
-    // Deserialize the received bytes into your struct
-    // This assumes the struct fields are uint8_t
-    int bezo1 = data[0];
-    int bezo2 = data[1];
-    int bezo3 = data[2];
-    int bezo4 = data[3];
-    int alcohol = data[4];
-    // Do something with the received data, e.g., update UI
-    print('Received Data: $bezo1, $bezo2, $bezo3, $bezo4, $alcohol');
-    checkThreshold(bezo1);
-    checkThreshold(bezo2);
-    checkThreshold(bezo3);
-    checkThreshold(bezo4);
-    checkThreshold(alcohol);
-  }
-
-  void checkThreshold(int value) async {
-    if (value > threshold) {
-      await _showNotification();
-    }
-  }
-
-  Future<void> _showNotification() async {
-    // Check if notification permission is granted
-    if (!(await Permission.notification.isGranted)) {
-      // Notification permission not granted, show a message to the user
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content:
-              Text('Notification permission is required to receive alerts.'),
-        ),
-      );
-      return;
-    }
-    // Notification permission is granted, proceed with showing the notification
-    const AndroidNotificationDetails androidPlatformChannelSpecifics =
-        AndroidNotificationDetails(
-      'your channel id',
-      'your channel name',
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-    const NotificationDetails platformChannelSpecifics =
-        NotificationDetails(android: androidPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      'Threshold Exceeded',
-      'One of the values has exceeded 80%',
-      platformChannelSpecifics,
-    );
   }
 
   @override
   void dispose() {
-    deviceConnection?.cancel();
+    _adapterStateStateSubscription.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Test Bluetooth'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: _startScanning,
-              child: Text('Start Scanning'),
-            ),
-          ],
-        ),
-      ),
+    Widget screen = _adapterState == BluetoothAdapterState.on
+        ? const ScanScreen()
+        : BluetoothOffScreen(adapterState: _adapterState);
+
+    return MaterialApp(
+      color: Colors.lightBlue,
+      home: screen,
+      navigatorObservers: [BluetoothAdapterStateObserver()],
     );
+  }
+}
+
+//
+// This observer listens for Bluetooth Off and dismisses the DeviceScreen
+//
+class BluetoothAdapterStateObserver extends NavigatorObserver {
+  StreamSubscription<BluetoothAdapterState>? _adapterStateSubscription;
+
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    super.didPush(route, previousRoute);
+    if (route.settings.name == '/DeviceScreen') {
+      // Start listening to Bluetooth state changes when a new route is pushed
+      _adapterStateSubscription ??= FlutterBluePlus.adapterState.listen((state) {
+        if (state != BluetoothAdapterState.on) {
+          // Pop the current route if Bluetooth is off
+          navigator?.pop();
+        }
+      });
+    }
+  }
+
+  @override
+  void didPop(Route route, Route? previousRoute) {
+    super.didPop(route, previousRoute);
+    // Cancel the subscription when the route is popped
+    _adapterStateSubscription?.cancel();
+    _adapterStateSubscription = null;
   }
 }
